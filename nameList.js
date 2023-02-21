31d6abb25c8780596adff3b0ffe03cf06783ce2e
volandoresort <125952668+volandoresort@users.noreply.github.com>
2023-02-21 14:50:48 +0800

    Add files via upload

---
+++ nameList.js
@@ -0,0 +1,22 @@
+let jsondata = [
+  {
+    name: "廖采揚",
+    query: "leo",
+    gender: "博士",
+  },
+  {
+    name: "賴麒宇",
+    query: "becker",
+    gender: "先生",
+  },
+  {
+    name: "羅智華",
+    query: "kevin",
+    gender: "先生",
+  },
+  {
+    name: "江俊嶙",
+    query: "main",
+    gender: "先生",
+  },
+];
